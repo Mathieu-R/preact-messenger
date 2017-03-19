0ae2457f045f1bc0c05d65fb1a9dc727a0a08c45
Mathieu-R <mathieu0709@gmail.com>
2017-03-19 14:41:48 +0100

    fix

--- back/utils.js
+++ back/utils.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 /* Promisify readFile */
 export.readFile = file => {
     return fs.readFile(file, (err, data) => {
-            new Promise(resolve, reject) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(data);
+        new Promise(resolve, reject) => {
+            if (err) {
+                reject(err);
+                return;
             }
+            resolve(data);
+        }
     })
 }
